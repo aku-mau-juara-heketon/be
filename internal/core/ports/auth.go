459584ba2eda665508a353df95d6dcd701bc6515
Jonathan0823 <eguinjonathan08@gmail.com>
2025-12-03 21:41:38 +0700

    refactor(core): split ports into user and auth interfaces

---
+++ internal/core/ports/auth.go
@@ -0,0 +1,11 @@
+package ports
+
+import (
+	"context"
+)
+
+type AuthService interface {
+	Login(ctx context.Context, email, password string) (string, string, error) // AccessToken, RefreshToken, Error
+	Register(ctx context.Context, email, password string) error
+	RefreshToken(ctx context.Context, refreshToken string) (string, string, error)
+}
--- internal/core/ports/user.go
+++ internal/core/ports/user.go
@@ -10,9 +10,3 @@ type UserRepository interface {
 	GetByEmail(ctx context.Context, email string) (*domain.User, error)
 	GetByID(ctx context.Context, id uint) (*domain.User, error)
 }
-
-type AuthService interface {
-	Login(ctx context.Context, email, password string) (string, string, error) // AccessToken, RefreshToken, Error
-	Register(ctx context.Context, email, password string) error
-	RefreshToken(ctx context.Context, refreshToken string) (string, string, error)
-}
